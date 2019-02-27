e436bbafd7f42187ada649c5e597059c3510f4ee
Colinkai <463790370@qq.com>
2019-02-27 20:08:42 +0800

    update

---
+++ CMS_1_settings.py
@@ -0,0 +1,7 @@
+import pygame
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (130,130,130)
+        self.background = pygame.image.load("Aircraft Material/background_1.gif")
